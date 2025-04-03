9d96f93c70721cf373ab24614d9008f8614fbf62
Niraj0051 <nirajpingale902894@gmail.com>
2025-04-04 00:52:12 +0530

    issue resolved

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 import AddNewInterview from './_components/AddNewInterview'
-import Interviewlist from './_components/Interviewlist'
+import Interviewlist from './_components/InterviewList'
 import Link from 'next/link'
 import { BarChart } from 'lucide-react'
 
